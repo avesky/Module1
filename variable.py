d58a075623b17dd9afa01ad536b20d415bd01986
avesky <avesky@gmail.com>
2021-08-29 16:01:12 -0500

    adding variable.py

---
+++ variable.py
@@ -0,0 +1,18 @@
+"""
+Program: variable.py
+Author: Andy Volesky
+Last date modified: 08/29/2021
+
+The purpose of this program Write a small program that declares 3 variables
+quantity (integer), item (string) and size (float-point number).
+Set them to your favorite numbers and a random item.
+"""
+
+# setting quantity variable
+quantity = 56
+# setting item variable
+item = 'Snakes'
+# setting size variable
+size = 10.7
+# setting constant variable
+PRICE = 3.50
