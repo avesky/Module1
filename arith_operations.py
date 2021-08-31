8c024f562e1c5af381afc4c09d65a7f1bd99b547
avesky <avesky@gmail.com>
2021-08-30 22:37:39 -0500

    Module1 Topic3 arithmetic operations

---
+++ arith_operations.py
@@ -0,0 +1,36 @@
+"""
+Program: variable.py
+Author: Andy Volesky
+Last date modified: 08/29/2021
+
+The purpose of this program Write a small program that declares 3 variables
+quantity (integer), item (string) and size (float-point number).
+Set them to your favorite numbers and a random item.
+"""
+# Addition
+add = 7+2
+print(add)
+
+# Subraction
+sub = 7-3
+print(sub)
+
+# Multiplication
+multipy = 7*2
+print(multipy)
+
+# Expononet
+exp = 5**2
+print(exp)
+
+# Division
+div = 6/2
+print(div)
+
+# Modulus, remainder of integer division
+mod = 6%2
+print(mod)
+
+# Floor division
+floor_div = 10//4
+print(floor_div)
