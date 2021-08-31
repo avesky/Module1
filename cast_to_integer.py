74ddd65da6c8508afdf51b1932a3478d957c6162
avesky <avesky@gmail.com>
2021-08-30 22:56:41 -0500

    Module1 Topic3 casting assignment

---
+++ cast_to_integer.py
@@ -0,0 +1,20 @@
+"""
+Program: cast_to_integer.py
+Author: Andy Volesky
+Last date modified: 08/30/2021
+
+The purpose of this program is to accept any input,
+convert to a integer type and output the integer.
+"""
+
+# input of 7
+input_a = int(7)
+print(input_a)
+
+# input of 9.99
+input_b = int(9.99)
+print(input_b)
+
+# input of 'randomstring'
+input_c = int('randomstring')
+print(input_c)
